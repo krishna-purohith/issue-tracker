6d10174a9d1d563e5385b9baf868778d7adad77f
Krishna Purohith <purohithkrishna.1@gmail.com>
2025-06-05 11:48:35 +0530

    Build an api for updating issues

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,11 +1,12 @@
+import { issueSchema } from "@/app/validationSchemas";
 import { prisma } from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import { createIssueSchema } from "../../validationSchemas";
+
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
 
-    const validation = createIssueSchema.safeParse(body);
+    const validation = issueSchema.safeParse(body);
     if (!validation.success)
         return NextResponse.json(validation.error.format(), { status: 400 });
     
