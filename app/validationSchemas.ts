6d30d99e20824748609436b1f8fcf25900be354a
Krishna Purohith <purohithkrishna.1@gmail.com>
2025-07-15 11:42:17 +0530

    Enhance the api to update the issues

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,6 +1,25 @@
 import { z } from "zod";
 
 export const issueSchema = z.object({
-    title: z.string().min(1, 'Title is required.').max(255),
-    description: z.string().min(1, 'Description is required')
+  title: z.string().min(1, "Title is required.").max(255),
+  description: z.string().min(1, "Description is required").max(65535),
+});
+
+// title, description
+// assignedToUserId
+// status
+
+export const patchIssueSchema = z.object({
+  title: z.string().min(1, "Title is required").max(255).optional(),
+  description: z
+    .string()
+    .min(1, "Description is required")
+    .max(655355)
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "AssignedToUserId is requried")
+    .max(255)
+    .optional()
+    .nullable(),
 });
