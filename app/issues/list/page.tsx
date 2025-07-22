c2760f9b238862d95377fe03815237863dfb811b
Krishna Purohith <purohithkrishna.1@gmail.com>
2025-07-22 14:01:52 +0530

    Sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -26,8 +26,16 @@ const IssuesPage = async ({ searchParams }: Props) => {
   const status = statusses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
+
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: { status },
+    orderBy,
   });
 
   return (
