74a609ee59367717414d37990c6568cb3221693e
Krishna Purohith <purohithkrishna.1@gmail.com>
2025-07-23 18:24:48 +0530

    Build the IssueChart component

---
+++ app/IssueChart.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { Card } from "@radix-ui/themes";
+import {
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
+import React from "react";
+
+interface Props {
+  open: number;
+  inProgress: number;
+  closed: number;
+}
+
+const IssueChart = ({ open, inProgress, closed }: Props) => {
+  const data = [
+    { label: "Open", value: open },
+    { label: "In Progress", value: inProgress },
+    { label: "Closed", value: closed },
+  ];
+
+  return (
+    <Card>
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart width={600} height={300} data={data}>
+          <XAxis dataKey="label" />
+          <YAxis />
+          <Tooltip />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <Bar
+            dataKey="value"
+            barSize={60}
+            style={{ fill: "var(--accent-9)" }}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </Card>
+  );
+};
+
+export default IssueChart;
